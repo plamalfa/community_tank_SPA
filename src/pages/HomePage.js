caa1d42a23ce2813b9160428b86d016749abd491
Peter LaMalfa <lamalfa.peter@gmail.com>
2020-04-12 23:37:54 -0400

    added bootstrap and sticky nav

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,13 +4,23 @@ import '../styles/Homepage.css'
 
 export default function Homepage() {
   return (
-    <div className="container">
-      <h1>Freshwater Aquarium Guide for Beginners</h1>
-      <header>
-        <Link to="/gettingStarted">Getting Started </Link> | <Link to="/species">Fish</Link>
-      </header>
-      <br />
-      Welcome! Whether you are brand new to the fishkeeping hobby or returning after a few years, this guide is a great resource for learning how to care for your aquatic friends. A community tank is a great option for people who want to enjoy a variety of different fish. In addition to covering tank setup and maintenance, I’ve focused on a handful of popular fish species known for their peaceful nature and heartiness - perfect for a beginner. With the right knowledge and a little patience, fishkeeping can be extremely fun and rewarding.
+    <div class='container-sm'>
+      <nav class='navbar fixed-top'>
+        <h3 class=''>Pete's Aquarium Guide</h3>
+        <Link to="/gettingStarted">Getting Started</Link><Link to="/species">Fish</Link><Link to="/species">Fish</Link><Link to="/species">Fish</Link>
+      </nav>
+      <section>
+        <p>Welcome! Whether you are brand new to the fishkeeping hobby or returning after a few years, this guide is a great resource for learning how to care for your aquatic friends. A community tank is a great option for people who want to enjoy a variety of different fish. In addition to covering tank setup and maintenance, I’ve focused on a handful of popular fish species known for their peaceful nature and heartiness - perfect for a beginner. With the right knowledge and a little patience, fishkeeping can be extremely fun and rewarding.</p>
+      </section>
+      <section>
+        <p>Welcome! Whether you are brand new to the fishkeeping hobby or returning after a few years, this guide is a great resource for learning how to care for your aquatic friends. A community tank is a great option for people who want to enjoy a variety of different fish. In addition to covering tank setup and maintenance, I’ve focused on a handful of popular fish species known for their peaceful nature and heartiness - perfect for a beginner. With the right knowledge and a little patience, fishkeeping can be extremely fun and rewarding.</p>
+      </section>
+      <section>
+        <p>Welcome! Whether you are brand new to the fishkeeping hobby or returning after a few years, this guide is a great resource for learning how to care for your aquatic friends. A community tank is a great option for people who want to enjoy a variety of different fish. In addition to covering tank setup and maintenance, I’ve focused on a handful of popular fish species known for their peaceful nature and heartiness - perfect for a beginner. With the right knowledge and a little patience, fishkeeping can be extremely fun and rewarding.</p>
+      </section>
+      <section>
+        <p>Welcome! Whether you are brand new to the fishkeeping hobby or returning after a few years, this guide is a great resource for learning how to care for your aquatic friends. A community tank is a great option for people who want to enjoy a variety of different fish. In addition to covering tank setup and maintenance, I’ve focused on a handful of popular fish species known for their peaceful nature and heartiness - perfect for a beginner. With the right knowledge and a little patience, fishkeeping can be extremely fun and rewarding.</p>
+      </section>
     </div>
   )
 }
